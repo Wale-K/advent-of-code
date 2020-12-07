c83565eaf84e8782ba108a9290c770529a8ff00e
Wale-K <olawale.k17@gmail.com>
2020-12-07 23:32:35 +0000

    Day 7 part 1, complete.

--- day6/day6-2.js
+++ day6/day6-2.js
@@ -1,56 +1,50 @@
-const fs = require("fs");
+const getNewSum = () => {
+  const fs = require("fs");
 
-const lines = fs
-  .readFileSync("puzzle.txt", { encoding: "utf-8" })
-  .split("\n\n")
-  .filter((x) => x);
+  const lines = fs
+    .readFileSync("puzzle.txt", { encoding: "utf-8" })
+    .split("\n\n")
+    .filter((x) => x);
 
-const brokenGroups = lines.map((elem) => {
-  elem = elem.replace(/\n/g, " ").split("");
-  return elem;
-});
-
-let newYesAnswers = [];
-let totalSumOfYesAnswers = 0;
-let groupsByNumber = [];
-let groupNumber = 1;
-let tally = 0;
-let pastAnswer = "";
-
-let solve = 0;
-
-brokenGroups.forEach((group, boo) => {
-  group.forEach((person) => {
-    if (person === " ") {
-      groupNumber++;
-    }
-
-    if (person !== " ") {
-      newYesAnswers.push(person);
-      totalSumOfYesAnswers++;
-    }
+  const brokenGroups = lines.map((elem) => {
+    elem = elem.replace(/\n/g, " ").split("");
+    return elem;
   });
 
-  if (groupNumber === 1) {
-    solve += newYesAnswers.length;
-  } else {
-    newYesAnswers.sort().forEach((answer) => {
-      tally++;
-      if (answer !== pastAnswer) {
-        tally = 0;
-        tally++;
-      }
-      if (tally === groupNumber) {
-        solve++;
-      }
+  let newYesAnswers = [];
+  let groupNumber = 1;
+  let tally = 0;
+  let pastAnswer = "";
+  let totalYesAnswers = 0;
 
-      pastAnswer = answer;
+  brokenGroups.forEach((group) => {
+    group.forEach((person) => {
+      if (person === " ") {
+        groupNumber++;
+      } else if (person !== " ") {
+        newYesAnswers.push(person);
+      }
     });
-  } // end of the else
 
-  newYesAnswers = [];
-  groupNumber = 1;
-  tally = 0;
-});
+    if (groupNumber === 1) {
+      totalYesAnswers += newYesAnswers.length;
+    } else {
+      newYesAnswers.sort().forEach((answer) => {
+        tally++;
+        if (answer !== pastAnswer) {
+          tally = 0;
+          tally++;
+        }
+        if (tally === groupNumber) {
+          totalYesAnswers++;
+        }
+        pastAnswer = answer;
+      });
+    }
 
-console.log(solve);
+    newYesAnswers = [];
+    groupNumber = 1;
+    tally = 0;
+  });
+  return totalYesAnswers;
+};
---
+++ day7/day7-1.js
@@ -0,0 +1,69 @@
+const fs = require("fs");
+
+const lines = fs
+  .readFileSync("puzzle.txt", { encoding: "utf-8" })
+  .split("\n")
+  .filter((x) => x);
+
+const allBags = {};
+
+lines.forEach((line) => {
+  const containedBags = line.match(/(?<=\d\s)\w+\s\w+/gm);
+  if (containedBags) {
+    allBags[line.match(/\w+\s\w+/m)] = containedBags;
+  } else {
+    allBags[line.match(/\w+\s\w+/m)] = [];
+  }
+});
+
+let totalBags = 0;
+let extraColours = [];
+let searchColours = [];
+
+// Object.keys(allBags).forEach(colourKey => {
+
+//   const containsSg = allBags[colourKey].includes("shiny gold");
+//   if (allBags[colourKey]) {
+//     totalBags++;
+//     searchColours.push(colourKey)
+//   }
+// });
+
+// searchColours.forEach(elem => {
+//   const containsElem = allBags[colourKey].includes(elem);
+//   const alreadyHasShinyGold = allBags[colourKey].indexOf("shiny gold") === -1
+//   if (allBags[colourKey] !== null && containsElem && alreadyHasShinyGold) {
+//     totalBags++
+//   }
+// })
+
+// console.log(totalBags)
+
+const countShiny = (arr) => {
+  // isolate first argument
+  // first is the focus bag that's being searched for (colour in focus)
+  // remainder is to be used in the next recursive function
+  const [first, ...remainder] = arr;
+  // here we want to iterate over allBags and find which bag contains the colour in focus.
+  Object.keys(allBags).forEach((colour) => {
+    // check if colour in focus is in the "colour" array
+    if (allBags[colour].includes(first)) {
+      // if (!extraColours.includes(colour)) {
+
+      totalBags++;
+      remainder.push(colour);
+      // extraColours.push(colour)
+      delete allBags[colour];
+      // }
+      // extraColours.push(colour)
+      // if any other colour contains this colour
+    }
+  });
+
+  if (remainder.length > 0) {
+    countShiny(remainder);
+  }
+};
+
+countShiny(["shiny gold"]);
+console.log(totalBags);
